f28cbeabf72bf146df57ee62fd0423cdb40d89c3
LYZ-Li <liyuanzhemail@gmail.com>
2024-02-16 03:14:04 +0100

    v2.2.0 add pseudo main

--- pseudoMain.py
+++ pseudoMain.py
@@ -1,34 +1,28 @@
-# import paho.mqtt.client as mqtt
-# import time
-# import os
-
-# ## Mosquitto 
-# mqttBroker = "localhost"
-# mqttPort = 1883
-# mqttTopic = "wenglor_trigger"
-
-
-# client = mqtt.Client("publisher")
-
-# client.connect(os.environ.get("MQTT_IP",mqttBroker),os.environ.get("MQTT_PORT",mqttPort))
-
-# trigger = True
-# client.publish(mqttTopic, trigger)
-
-# time.sleep(1)
-
-# trigger = False
-# client.publish(mqttTopic, trigger)
-
-# client.disconnect()
-
-
-
 import time
+import socket
+import os
+
 trigger = False
+def wenglorInit():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    server.bind(('0.0.0.0', os.environ.get("MQTT_PORT",9999)))
+    
+    server.listen(1)
+    print('waiting for connection ...')
+    while True:
+        sock, _ = server.accept()
+        return sock
+
+def wenglor(tcp, trigger):
+    tcp.send(str(trigger).encode('utf-8'))
+
+
 
 def main():
-    wenglorInit()
+    tcp = wenglorInit()
     trigger = True
-    wenglor(trgger)
+    wenglor(tcp, trigger)
     time.sleep(5)
+    trigger = False
+    wenglor(tcp, trigger)
